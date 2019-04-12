57b43e8c026ba722443ff315fdfceac0db5ae6df
Suresh Kaushik <suresh.kau05@gmail.com>
2019-04-12 15:23:23 +0530

    #04122019 Added new project for WebAPI

---
+++ ProductsApp/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using ProductsApp.Models;
+
+namespace ProductsApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        //
+        // GET: /Products/
+        Product[] products = new Product[]
+        {
+            new Product{Id =1, Name = "Tomato Soup", Category = "Groceries", Price=1},
+            new Product{Id =2, Name = "Hammer", Category = "Hardware", Price=2},
+            new Product{Id =3, Name = "Kids Toy", Category = "Toys", Price=3}
+        };
+
+        public IEnumerable<Product > GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+	}
+}
